8a98c9428613bf650032d9c00db139ac4ac5299d
Ricardo Fernandez Vilca <ricardofervil@gmail.com>
2026-04-23 11:32:42 +0200

    Fix: Añadir https:// a la URL de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'finance-tracker-backend-production-2936.up.railway.app/api'
+  apiUrl: 'https://finance-tracker-backend-production-2936.up.railway.app/api'
 };
