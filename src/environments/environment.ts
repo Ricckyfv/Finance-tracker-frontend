be11b1b6e274dcceb239a3098589385fd83a99e1
Ricardo Fernandez Vilca <ricardofervil@gmail.com>
2026-06-01 10:31:29 +0200

    change: poner enlace de back en Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://finance-tracker-backend-production-2936.up.railway.app/api'
+  // apiUrl: 'https://finance-tracker-backend-production-2936.up.railway.app/api'
+  apiUrl: 'https://finance-tracker-backend-0ump.onrender.com/api'
 };
